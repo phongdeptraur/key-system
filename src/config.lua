f1824fdba2a6ad90dca93e2b8a5abc728efac5a3
phongdeptraur <133789748+phongdeptraur@users.noreply.github.com>
2026-01-05 10:59:22 +0700

    Update config.lua

--- src/config.lua
+++ src/config.lua
@@ -1,5 +1,5 @@
 return {
     API = "https://script.google.com/macros/s/AKfycbzVpZeJSMmrPGL1kSGY1O4M-s59mOQcYwPiSFUcDxQXqFUfRKEx8Kve-gBF2uRsZ4rQ/exec", -- dán URL web app của bạn
-    MAIN_RAW = "https://raw.githubusercontent.com/phongdeptraur/key-system/main/src/main.lua",
+    MAIN_RAW = "https://raw.githubusercontent.com/phongdeptraur/key-system/main/src/a9k3m7q2x8b5c1z0d4f.lua",
     CACHE_TTL_SEC = 30
 }
